bc3c53dd03539c714031a6c1e29e4df7357ef002
sumit0630 <sumit0630@users.noreply.github.com>
2021-04-20 11:14:41 +0530

    initial changes

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Sumit Dahiya on 20/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func setUI(){
+        print("hello world")
+    }
+
+}
+
